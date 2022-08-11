750b5fc717ece476403bb088ae43313fae6808f1
kabirajk <kabirajkannan26@gmail.com>
2022-08-12 00:07:08 +0530

    first_commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var bg = document.querySelector('.home');
+var text = document.querySelector('.shadow');
+var zohologo = document.querySelector('.zohologo');
+function change(){
+    bg.style.backgroundImage="url('ZohoTower.jpg')";
+    zohologo.style.visibility='visible';
+    text.style.color='white';
+}
+function out(){
+    bg.style.backgroundImage="linear-gradient(90deg,rgb(56, 137, 137),rgb(246,170,255))";
+    zohologo.style.visibility='hidden';
+    text.style.color='black';
+}
